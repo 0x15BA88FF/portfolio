4a97cd206bf795ad284fce8814190c9746ba8fef
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2024-11-15 17:23:01 +0000

    api/projects modify: add aditional project information

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -15,12 +15,12 @@ export default function handler(req, res) {
           name: "Visual Json",
           images: ["/projects/visual_json.png"],
           description: "A simple development tool i built and use to run GET requests and represent JSON respones into a graphical tree.",
-          link: "https://example.com/project-three"
+          link: 'https://github.com/0x15BA88FF/Visual-JSON'
         },
         {
           name: "Setifi",
           images: ["/projects/setifi_module.png"],
-          description: "A Node.js library used to validate passwords and test their strength complete with documentation and showcase.",
+          description: 'A node package to make your data certification easier and secure users from brute force attacks, used to validate passwords and test their strength complete with documentation and showcase.',
           link: "https://github.com/0x15BA88FF/setifi"
         },
         {
@@ -31,3 +31,39 @@ export default function handler(req, res) {
       ]
   )
 }
+
+//  {
+//    name: 'color-versioning',
+//    description: 'No description provided.',
+//    link: 'https://github.com/0x15BA88FF/color-versioning'
+//  },
+//  {
+//    name: 'devault',
+//    description: 'Manage local projects and repositories like a git server',
+//    link: 'https://github.com/0x15BA88FF/devault'
+//  },
+//  {
+//    name: 'karaoke.py',
+//    description: 'Karaoke is a lightweight Python library designed to facilitate interactions with song LRC (Lyric) files.',
+//    link: 'https://github.com/0x15BA88FF/karaoke.py'
+//  },
+//  {
+//    name: 'Presec-AI-Companion',
+//    description: 'No description provided.',
+//    link: 'https://github.com/0x15BA88FF/Presec-AI-Companion'
+//  },
+//  {
+//    name: 'thalamus',
+//    description: 'A LLM driven engine that performs actions and commands',
+//    link: 'https://github.com/0x15BA88FF/thalamus'
+//  },
+//  {
+//    name: 'Workin',
+//    description: 'No description provided.',
+//    link: 'https://github.com/0x15BA88FF/Workin'
+//  },
+//  {
+//    name: 'yt-data-drive',
+//    description: 'Command-line tool designed to efficiently encode and decode files for video archiving.',
+//    link: 'https://github.com/0x15BA88FF/yt-data-drive'
+//  }
